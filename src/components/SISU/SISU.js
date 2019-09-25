b1ab91a771f6ab29d35ecaa19c086cb94a7391e4
Nathan Freney <nathan.freney@gmail.com>
2019-09-24 20:18:37 -0600

    adding a SISU component

---
+++ src/components/SISU/SISU.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SISU extends Component {
+    render() {
+        return (
+            <div>
+                <p>Placeholder</p>
+            </div>
+        )
+    }
+}
+
+export default SISU;
