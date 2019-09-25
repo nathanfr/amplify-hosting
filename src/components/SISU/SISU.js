ae03cf6b6e4b6f3aa5876eb5128ca0b37758bfc2
Nathan Freney <nathan.freney@gmail.com>
2019-09-24 21:17:40 -0600

    added a background image

--- src/components/SISU/SISU.js
+++ src/components/SISU/SISU.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 
+// Styling
+import './SISU.css';
+
 class SISU extends Component {
     render() {
         return (
             <div className="SISUContainer">
-                <p>Placeholder</p>
             </div>
         )
     }
